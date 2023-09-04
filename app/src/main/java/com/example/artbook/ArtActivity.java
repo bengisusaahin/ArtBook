cc9e5707d9aa0ea357ece0cab6481c223fad6d84
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-09-05 01:13:18 +0300

    Created menu

--- app/src/main/java/com/example/artbook/ArtActivity.java
+++ app/src/main/java/com/example/artbook/ArtActivity.java
@@ -5,19 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.artbook.databinding.ActivityArtBinding;
+
 public class ArtActivity extends AppCompatActivity {
+
+    private ActivityArtBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_art);
+        //setContentView(R.layout.activity_art);
+        binding = ActivityArtBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 
     public void save(View view){
 
     }
 
-    public void SelectImage(View view){
-        
+    public void selectImage(View view){
+
     }
 }
